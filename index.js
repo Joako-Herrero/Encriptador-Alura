9957af490d625b38f0dd1fde5d4e412d4e54e6de
Joako-Herrero <joaquinherrero13@gmail.com>
2023-01-19 13:28:52 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function getTextEncriptar(){
+	let str = document.getElementById("inputText");
+	let string = str.value;
+	encriptador(string);
+}
+
+function encriptador(string){
+
+	let newString = string.split('a').join('ai').split('e').join('enter').split('i').join('imes').split('o').join('ober').split('u').join('ufat').split('aimes').join('ai');
+	document.getElementById("output").style.display = "none";
+	document.getElementById("resultado").style.display = "inline";
+	return document.getElementById("resultado").innerHTML = newString;
+
+}
+
+
+
+function getTextDesencriptar(){
+	let str = document.getElementById("inputText");
+	let string = str.value;
+	desencriptador(string);
+}
+
+function desencriptador(string){
+
+	let newString = string.split('ai').join('a').split('enter').join('e').split('imes').join('i').split('ober').join('o').split('ufat').join('u');
+	document.getElementById("output").style.display = "none";
+	document.getElementById("resultado").style.display = "inline";
+	return document.getElementById("resultado").innerHTML = newString;
+
+}
+
+function copiar(){
+	
+}
